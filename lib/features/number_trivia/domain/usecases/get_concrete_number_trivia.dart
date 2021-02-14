0ba10ed0b0989cdff77b4a23b03d963e12799f36
EtNinja <annealmd@gmail.com>
2021-02-14 00:07:10 -0300

    :white_check_mask: test: NumberTriviaRepository

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:number/core/error/failures.dart';
+import 'package:number/features/number_trivia/data/repositories/number_trivia_repository.dart';
+import 'package:number/features/number_trivia/domain/entities/number_trivia.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+  GetConcreteNumberTrivia(
+    this.repository,
+  );
+
+  Future<Either<Failure, NumberTrivia>> execute({@required int number}) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+}
