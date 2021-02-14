ba6faac3435c01eb0b136a3509fc2beb087cadfa
EtNinja <annealmd@gmail.com>
2021-02-14 14:39:05 -0300

    :hammer: refactor: data domain
    moved contract repository form data to domain

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:number/core/error/failures.dart';
-import 'package:number/features/number_trivia/data/repositories/number_trivia_repository.dart';
+import 'package:number/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:number/features/number_trivia/domain/entities/number_trivia.dart';
 
 class GetConcreteNumberTrivia {
@@ -10,7 +10,9 @@ class GetConcreteNumberTrivia {
     this.repository,
   );
 
-  Future<Either<Failure, NumberTrivia>> execute({@required int number}) async {
+  Future<Either<Failure, NumberTrivia>> execute({
+    @required int number,
+  }) async {
     return await repository.getConcreteNumberTrivia(number);
   }
 }
