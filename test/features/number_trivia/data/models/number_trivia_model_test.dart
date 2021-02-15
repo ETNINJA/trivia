1a86358f131bc6ba151bada183325adea09015e0
EtNinja <annealmd@gmail.com>
2021-02-14 21:50:18 -0300

    :white_check_mark: test: fromJSON
    created folder fixture in test
    tested the model (data/models/number_trivia_model)

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,11 +1,49 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:number/features/number_trivia/domain/entities/number_trivia.dart';
 
-void main() {
-  final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'test text');
+import '../../../../fixtures/fixture_reade.dart';
 
-  test('should be  a subclass of NumberTrivia entity', () async {
-    expect(tNumberTriviaModel, isA<NumberTrivia>());
+void main() {
+  final tNumberTriviaModel = NumberTriviaModel(
+    number: 4,
+    text: '4 is the number of planck volumes in the observable universe.',
+  );
+
+  test(
+    'should be  a subclass of NumberTrivia entity',
+    () async {
+      expect(tNumberTriviaModel, isA<NumberTrivia>());
+    },
+  );
+
+  group('fromJson', () {
+    test(
+      'should return a valid model when the json is integer',
+      () async {
+        //arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia.json'));
+        //act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        //assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
+
+    test(
+      'should return a valid model when the json is double',
+      () async {
+        //arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia_double.json'));
+        //act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        //assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
   });
 }
