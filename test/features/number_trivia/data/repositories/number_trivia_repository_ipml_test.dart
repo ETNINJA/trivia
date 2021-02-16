55ae7e8d3e35abfb6e45db5960e769d827e6bbb1
EtNinja <annealmd@gmail.com>
2021-02-15 22:33:40 -0300

    :white_check_mark: test: remote data source

--- test/features/number_trivia/data/repositories/number_trivia_repository_ipml_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_ipml_test.dart
@@ -1,6 +1,7 @@
 import 'package:number/core/platform/network_info.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:dartz/dartz.dart';
 import 'package:number/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:number/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:number/features/number_trivia/data/models/number_trivia_model.dart';
@@ -31,21 +32,68 @@ void main() {
     );
   });
 
-  group('getConcreteNumberTrivia', () {
-    // DATA FOR THE MOCKS AND ASSERTIONS
-    // We'll use these three variables throughout all the tests
-    final tNumber = 1;
-    final tNumberTriviaModel =
-        NumberTriviaModel(number: tNumber, text: 'test trivia');
-    final NumberTrivia tNumberTrivia = tNumberTriviaModel;
+  group(
+    'getConcreteNumberTrivia',
+    () {
+      // DATA FOR THE MOCKS AND ASSERTIONS
+      // We'll use these three variables throughout all the tests
+      final tNumber = 1;
+      final tNumberTriviaModel =
+          NumberTriviaModel(number: tNumber, text: 'test trivia');
+      final NumberTrivia tNumberTrivia = tNumberTriviaModel;
 
-    test('should check if the device is online', () {
-      //arrange
-      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
-      // act
-      repository.getConcreteNumberTrivia(tNumber);
-      // assert
-      verify(mockNetworkInfo.isConnected);
-    });
-  });
+      test(
+        'should check if the device is online',
+        () {
+          //arrange
+          when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+          // act
+          repository.getConcreteNumberTrivia(tNumber);
+          // assert
+          verify(mockNetworkInfo.isConnected);
+        },
+      );
+      group('device is online', () {
+        // This setUp applies only to the 'device is online' group
+        setUp(() {
+          when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+        });
+
+        test(
+          'should return remote data when the call to remote data source is successful',
+          () async {
+            // arrange
+            when(mockRemoteDataSource.getConcreteNumberTrivia(any))
+                .thenAnswer((_) async => tNumberTriviaModel);
+            // act
+            final result = await repository.getConcreteNumberTrivia(tNumber);
+            // assert
+            verify(mockRemoteDataSource.getConcreteNumberTrivia(tNumber));
+            expect(result, equals(Right(tNumberTrivia)));
+          },
+        );
+      });
+
+      group('device is offline', () {
+        setUp(() {
+          when(mockNetworkInfo.isConnected).thenAnswer((_) async => false);
+        });
+
+        test(
+          'should return last locally cached data when the cached data is present',
+          () async {
+            // arrange
+            when(mockLocalDataSource.getLastNumberTrivia())
+                .thenAnswer((_) async => tNumberTriviaModel);
+            // act
+            final result = await repository.getConcreteNumberTrivia(tNumber);
+            // assert
+            verifyZeroInteractions(mockRemoteDataSource);
+            verify(mockLocalDataSource.getLastNumberTrivia());
+            expect(result, equals(Right(tNumberTrivia)));
+          },
+        );
+      });
+    },
+  );
 }
